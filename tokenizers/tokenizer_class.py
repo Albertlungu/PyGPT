0e56509e720b52c0dee567c06240c26233592d5b
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2025-10-17 11:33:53 -0400

    Updated tokenizer_class to include debugging messages and infrastructure to select smaller sub-section of dataset

--- tokenizers/tokenizer_class.py
+++ tokenizers/tokenizer_class.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import re
 from tqdm import tqdm
 
+
 def extract_wiki_text(xml_path, output_path):
     """
     Extract text from a wiki XML dump and write it to a file.
@@ -33,15 +34,14 @@ def extract_wiki_text(xml_path, output_path):
                     cleaned = re.sub(pattern, "", cleaned)
                 f.write(cleaned + "\n")
 
-
 class BPETokenizer:
-    def __init__(self, input=32000):
+    def __init__(self, vocab_size = 32000):
         """
         Initializes a BPETokenizer object.
         If input is None, vocab size is set to 1000.
             OUTDATED --> No longer uses input, instead, uses harcoded value of 32k vocab size
         """
-        self.vocab_size = 32000 # int(np.sqrt(len(input))) # default vocab_size set to 32000 for a large dataset
+        self.vocab_size = vocab_size # int(np.sqrt(len(input))) # default vocab_size set to 32000 for a large dataset
         self.base_vocab_size = 256
         self.merges = {} # initializing merges dictionary
 
@@ -78,7 +78,7 @@ class BPETokenizer:
                 i += 1
         return new_input
     
-    def make_merges(self, input):
+    def make_merges(self, input, dataset_length):
         """
         Merge adjacent ids in a list of ids until the vocab size is reached. Why? This is to increase the vocab size. This is to compress more tokens into a a single token, making the context length more compact, and the model can remember more at a time.
         Args:
@@ -92,30 +92,42 @@ class BPETokenizer:
         merges = {}
         input = list(input)
         base_vocab_start = self.base_vocab_size
-        for i in tqdm(range(num_merges)): # iterating over the number of merges using tqdm in order to show progress bar
-            stats = self.get_stats(input) # getting stats
+        print("Starging merges now: ")
+        for i in tqdm(range(num_merges)): # iterating over the number of merges  num_merges//100
+            stats = self.get_stats(input[:dataset_length]) # getting stats
+            # print("Got stats")
             if not stats:
+                # print("Stats are empty, breaking")
                 break # safety check
             pair = max(stats, key=stats.get) # getting the pair with the highest count (most repeated pair)
+            # print("Most repeated pair: ", pair)
             idx = base_vocab_start + i
             # print(f"merging {pair} into a new token: {idx}")  
-            input = self.merge(input, pair, idx) # merging the most repeated pair into one token
+            input = self.merge(input[:dataset_length], pair, idx) # merging the most repeated pair into one token
             merges[pair] = idx
         self.merges = merges
         return input
 
 if __name__ == "__main__":
+
+    dataset_length = 1000000
+    vocab_size = 1000
+
     training_data = open("tokenizer_training_data/all_wiki_text.txt", "r").read() # reading training data from wiki file
+    print("Read training data")
 
     tokens = training_data.encode("utf-8") # turns raw text (strings) into utf-8 encoded bytes stored inside tokens variable
+    print("Encoded training data")
+    print(list(tokens)[:100])
 
-    tokenizer = BPETokenizer(tokens) # isntancing the tokenizer class with tokens as the training data
+    tokenizer = BPETokenizer(vocab_size) # instancing the tokenizer class with tokens as the training data
+    print("Initialized tokenizer")
 
-    ids = tokenizer.make_merges(tokens) # calls make_merges function from inside tokenizer class and passes tokens
-
+    ids = tokenizer.make_merges(tokens, dataset_length) # calls make_merges function from inside tokenizer class and passes tokens
+    print("Merged tokens")
 
     # printing stats about the tokens for comparison
-    print("Original tokens length:", len(tokens))
+    print("Original tokens length:", len(tokens[:dataset_length]))
     print("Final ids length:", len(ids))
-    print(f"Compression ratio: {len(tokens) / len(ids):.2f}X")
+    print(f"Compression ratio: {len(tokens[:dataset_length]) / len(ids):.2f}X")
     # pass
