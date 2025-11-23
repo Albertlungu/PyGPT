b4bbfe88c1a14d040a06601f4b160c05f12f1189
Albertlungu <132965708+Albertlungu@users.noreply.github.com>
2025-11-23 18:06:17 -0500

    Add way to overwrite original tokens (eos and pad).

--- src/tokenizer/test_tokenizer.py
+++ src/tokenizer/test_tokenizer.py
@@ -1,10 +1,9 @@
 # Save as check_merges.py and run on your GPU machine
 import pickle
-from src.tokenizer.tokenizer_class import BPETokenizer
+from tokenizer_class import BPETokenizer
 
 with open('artifacts/tokenizer/tokenizer_alpaca.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
-    tokenizer._ensure_vocab()
 
 print(tokenizer.encode("Hello World"))
 
@@ -17,6 +16,10 @@ prompts = [
         "Instruction: Explain the importance of NASA's current mission to Mars.\nInput: \nOutput:"
     ]
 
+tokenizer.vocab[tokenizer.eos_token_id] = b"<EOS>"
+tokenizer.vocab[0] = b"<PAD>"
+
 print(tokenizer.encode(prompts))
 print(tokenizer.eos_token_id)
+print(tokenizer.decode([32000]))
 print(tokenizer.decode([0]))
